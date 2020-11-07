68f4aa1f470215ba1c756198bc9bcca6494d8288
Geert Dooms <geert.dooms@kbc.be>
2020-11-07 09:38:05 +0100

    lab04 - fixed nulls in ingredients

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/converter/CocktailDBResponseConverter.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/converter/CocktailDBResponseConverter.java
@@ -6,10 +6,7 @@ import com.ezgroceries.shoppinglist.model.Drink;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 public class CocktailDBResponseConverter {
@@ -28,26 +25,33 @@ public class CocktailDBResponseConverter {
 
     //todo -> refactor (cleaner; is there a better way to handle these 15 strings?)
     private Cocktail convertDrinkToCocktail (Drink drink) {
+
+        List<String> ingredients = new ArrayList<>();
+
+        ingredients.addAll(Arrays.asList(
+                drink.getStrIngredient1(),
+                drink.getStrIngredient2(),
+                drink.getStrIngredient3(),
+                drink.getStrIngredient4(),
+                drink.getStrIngredient5(),
+                drink.getStrIngredient6(),
+                drink.getStrIngredient7(),
+                drink.getStrIngredient8(),
+                drink.getStrIngredient9(),
+                drink.getStrIngredient10(),
+                drink.getStrIngredient11(),
+                drink.getStrIngredient12(),
+                drink.getStrIngredient13(),
+                drink.getStrIngredient14(),
+                drink.getStrIngredient15()));
+
+        ingredients.removeIf(Objects::isNull);
+
         return new Cocktail(UUID.randomUUID(),
                             drink.getStrDrink(),
                             drink.getStrGlass(),
                             drink.getStrInstructions(),
                             drink.getStrDrinkThumb(),
-                            Arrays.asList(
-                                drink.getStrIngredient1(),
-                                drink.getStrIngredient2(),
-                                drink.getStrIngredient3(),
-                                drink.getStrIngredient4(),
-                                drink.getStrIngredient5(),
-                                drink.getStrIngredient6(),
-                                drink.getStrIngredient7(),
-                                drink.getStrIngredient8(),
-                                drink.getStrIngredient9(),
-                                drink.getStrIngredient10(),
-                                drink.getStrIngredient11(),
-                                drink.getStrIngredient12(),
-                                drink.getStrIngredient13(),
-                                drink.getStrIngredient14(),
-                                drink.getStrIngredient15()));
+                            ingredients);
     }
 }
