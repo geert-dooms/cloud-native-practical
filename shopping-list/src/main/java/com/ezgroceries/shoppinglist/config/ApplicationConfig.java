cee9f4f4c4d7b0287eee9b22622c4cb496e1c2d8
Geert Dooms <geert.dooms@kbc.be>
2020-11-22 23:47:08 +0100

    lab08 - finished

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/config/ApplicationConfig.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/config/ApplicationConfig.java
@@ -1,5 +1,9 @@
 package com.ezgroceries.shoppinglist.config;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,5 +21,16 @@ public class ApplicationConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    //Custom tomcat configuration, we add an additional connector that allows http traffic next to https
+    @Bean
+    public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort) {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
     }
 }
