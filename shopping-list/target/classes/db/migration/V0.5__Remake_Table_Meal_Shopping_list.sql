c17b6e6960a7f8a1fbc889d21dbb4d35acbe1ffe
Geert Dooms <geert.dooms@kbc.be>
2020-12-06 14:04:57 +0100

    clean up target

--- shopping-list/target/classes/db/migration/V0.5__Remake_Table_Meal_Shopping_list.sql
+++
@@ -1,7 +0,0 @@
-drop table MEAL_SHOPPING_LIST;
-
-create table MEAL_SHOPPING_LIST (
-                                    MEAL_ID UUID REFERENCES meal (ID),
-                                    SHOPPING_LIST_ID UUID REFERENCES shopping_list (id),
-                                    CONSTRAINT MEAL_SHOPPING_LIST_PKEY PRIMARY KEY (MEAL_ID, SHOPPING_LIST_ID)
-);
--- shopping-list/target/classes/db/migration/V0.7__Update_Users_Data.sql
+++
@@ -1,4 +0,0 @@
-update users set password = '$2y$12$BeyP6h42OrfK6Vw.JDjlIO1yC2oYGzHz4vrm3aNWvTq1SaBEBLjPm'
-where enabled = true;
-
-INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_USER');
